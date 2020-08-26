785147417fb9e89ae56979602e07f2714dd5e7bf
Amanda Camacho <ajmandi94@gmail.com>
2020-08-26 12:47:10 -0500

    changes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const express = require('express')
 require('./db/mongoose')
 const userModule= require('./router/user')
-const answeModule = require('./router/answer')
 const questionModule = require('./router/question')
 
 const app= express()
 
 app.use(express.json())
 app.use(userModule)
-app.use(answeModule)
 app.use(questionModule)
 
 module.exports= app
--- src/router/question.js
+++ src/router/question.js
@@ -6,11 +6,18 @@ const router = new express.Router()
 
 
 router.get('/questions',async (req,res)=>{
-
     try{
         const questions = await Question.find()
+
+        if(!questions){
+            return res.status(404).send()
+        }
+
         res.send(questions)
     }catch(e){
         res.status(400).send(e)
     }
 })
+
+
+module.exports=router
