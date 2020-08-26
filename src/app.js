ed75abd37c2183017b4e7d5c2ed5a558f5848edb
Amanda Camacho <ajmandi94@gmail.com>
2020-08-26 12:24:53 -0500

    changes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const express = require('express')
 require('./db/mongoose')
 const userModule= require('./router/user')
 const answeModule = require('./router/answer')
-// const questionModule = require('./router/question')
+const questionModule = require('./router/question')
 
 const app= express()
 
 app.use(express.json())
 app.use(userModule)
 app.use(answeModule)
-// app.use(questionModule)
+app.use(questionModule)
 
 module.exports= app
 
