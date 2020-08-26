b2400d31ad30bf983b5ba4cde0dac3ad1bfbc814
Amanda Camacho <ajmandi94@gmail.com>
2020-08-26 18:01:57 -0500

    changes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ const questionModule = require('./router/question')
 const app= express()
 
 app.use(express.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(userModule)
 app.use(questionModule)
 
